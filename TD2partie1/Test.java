5919b06ad289bcf740c480c7002582ac69cb2e97
Filipedel <filipedelaunay@gmail.com>
2022-10-24 15:45:30 +0200

    m

--- TD2partie1/Test.java
+++ TD2partie1/Test.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args){
@@ -8,7 +11,31 @@ public class Test {
         Somme<Long> somme4 = (x,y)-> x + y;
 //TEST SOmme
         System.out.println(somme.somme(4,5)+ " "+ somme2.somme("a","b")+" "+somme3.somme(3.3,3.2));
+//TEST STRING
 
+        List<String> l2s = new ArrayList<>();
+        l2s.add("e1");
+        l2s.add("e2");
+        ToString<List<String>> tt = x -> {
+            StringBuilder build = new StringBuilder();
+            x.forEach((val)->{
+                build.append(val + ",");
+            });
+            return build.toString();
+        };
+        System.out.println(tt.display(l2s));
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("k1", 5);
+        hm.put("k2", 10);
+        ToString<HashMap<String, Integer>> toStringMap = x -> {
+            StringBuilder build = new StringBuilder();
+            x.forEach((key, val) -> {
+                build.append(String.format("%s : %d, ", key, val));
+            });
+            return build.toString();
+        };
+        System.out.println(toStringMap.display(hm));
 
     }
 
